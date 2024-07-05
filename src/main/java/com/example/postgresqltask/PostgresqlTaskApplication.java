9ee53a65359542376d81544be4dffe099db2565e
aminqurbanov <a.in.qurbanov@gmail.com>
2024-07-05 19:00:26 +0400

    "SQL"

---
+++ src/main/java/com/example/postgresqltask/PostgresqlTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgresqltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresqlTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresqlTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/postgresqltask/PostgresqlTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgresqltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresqlTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
